e99de1c646293a64612c8e6fbf1ca63e20b86542
saidMounaim <saidmounaim00@gmail.com>
2021-06-14 17:00:47 +0100

    Add Hero Section

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,33 @@
+import Image from 'next/image';
+
 const Hero = () => {
     return (
         <section className="hero-section">
+            <div className="container-hero">
 
+                <div className="content-hero">
+
+                    <div className="left-side">
+                        <h1>Task management and lists Tool</h1>
+                        <p>We believe that designing products and services in close
+                            partnership with our clients is the only way to have a real
+                            impact on their business.</p>
+                        <form>
+                            <div className="form-group">
+                                <input type="email" placeholder="Name@company.com" />
+                                <button type="submit" >Try it free</button>
+                            </div>
+                        </form>
+                    </div>
+
+
+                    <div className="right-side">
+                        <Image src="/hero.svg" layout="fill" />
+                    </div>
+
+                </div>
+
+            </div>
         </section>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 
+// Sections
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
     <>
@@ -7,6 +10,9 @@ export default function Home() {
         <title>Taskia | Landing Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Hero />
+
     </>
   )
 }
