6588549c18263d7a6f68a3a55fad0bdc4989b12d
saidMounaim <saidmounaim00@gmail.com>
2021-06-15 11:35:29 +0100

    Add Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,17 @@
 const Footer = () => {
     return (
         <footer>
-            <h1>Footer</h1>
+            <div className="header-footer">
+                <h3>Get better work done</h3>
+                <h5>See why millions of people across 195 countries use Dash.</h5>
+            </div>
+
+            <form>
+                <div className="form-group">
+                    <input type="email" placeholder="Name@company.com" />
+                    <button type="submit" >Try it free</button>
+                </div>
+            </form>
         </footer>
     )
 };
