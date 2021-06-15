a5ce7a08aa4e496aae4214f602589a0edcefde9e
saidMounaim <saidmounaim00@gmail.com>
2021-06-15 11:58:12 +0100

    Add Responsive Version

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css'
 import '../styles/style.css'
+import '../styles/responsive.css'
 
 function MyApp({ Component, pageProps }) {
   return (
